bf866fa887c554f193801f8b99a8cf549bd6a938
DenisKhudyakov <denis.hudyakov27@gmail.com>
2024-05-15 20:35:01 +0500

    project update

--- online_school/serializers.py
+++ online_school/serializers.py
@@ -5,6 +5,8 @@ from online_school.models import Course, Lesson
 
 class CourseSerializer(serializers.ModelSerializer):
     """Serializer for the Course model"""
+
+
     class Meta:
         model = Course
         fields = '__all__'
@@ -12,6 +14,7 @@ class CourseSerializer(serializers.ModelSerializer):
 
 class LessonSerializer(serializers.ModelSerializer):
     """Serializer for Lessons"""
+
     class Meta:
         model = Lesson
         fields = '__all__'
